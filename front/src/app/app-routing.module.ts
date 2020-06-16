5e06e86ae13903042d39d52de78c90f05bcfb2af
unknown <divyachundawat865@gmail.com>
2020-06-17 02:09:38 +0530

    frontend header created but routerlink isn't working by Divya

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WebsiteComponent } from './website/website.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { PublishComponent } from './publish/publish.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:WebsiteComponent,children:[
+    {path:'',component:HomeComponent},
+    {path:'signup',component:SignupComponent},
+    {path:'login',component:LoginComponent},
+    {path:'publish',component:PublishComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WebsiteComponent } from './website/website.component';
+import { HeaderComponent } from './header/header.component';
+import { LogoComponent } from './logo/logo.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { PublishComponent } from './publish/publish.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WebsiteComponent,
+    HeaderComponent,
+    LogoComponent,
+    HomeComponent,
+    SignupComponent,
+    LoginComponent,
+    PublishComponent
   ],
   imports: [
     BrowserModule,
